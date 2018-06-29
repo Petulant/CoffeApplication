62d79d31918042dd83a0d3f03dbfee641c0be199
Thembelihle <pmokhwebane@gmail.com>
2018-06-29 09:37:50 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DISABLED } from '@angular/forms/src/model';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  checked1;
+  checked2;
+  increment=0;
+   total=0;
+   Name;
+   Summary:number;
+   message : string;
+   price=8;
+
 
   constructor(public navCtrl: NavController) {
+   this.Summary=0;
+  
+  }
+   myButton(){
+   this.Summary=1;
+   this.total=this.price *  this.increment;
+ 
+   }
+  ADD()
+  {
+    if(this.increment>=0){
+      this.increment+=1;
+  
+    }
 
   }
 
+  MINUS()
+  {
+
+    if(this.increment>=1){
+      this.increment-=1;
+    }
+
+  }
+  Reset(){
+    this.Summary=0;
+    this.checked1=false;
+    this.checked2=false;
+    this.increment=0;
+    this.Name=" ";
+    this.total=0;
+
+  }
+ 
+ 
 }
